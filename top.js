3ed3f6ffae554f54a1b3f112eba56737a6a52b4e
Raman Kumar <raman.kumar.rk7oo7@gmail.com>
2023-07-07 10:55:07 +0530

    zeal-society

--- top.js
+++ top.js
@@ -1,7 +1,4 @@
-// Get the button
 let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,8 +8,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
