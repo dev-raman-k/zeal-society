7805a700412205b2014982c81795056fa2463a46
Raman Kumar <raman.kumar.rk7oo7@gmail.com>
2023-07-07 23:48:03 +0530

    zeal-society

--- top.js
+++ top.js
@@ -9,6 +9,5 @@ function scrollFunction() {
   }
 }
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
